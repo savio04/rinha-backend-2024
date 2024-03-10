2eee0f4062c4ae97f8b1cd6975759db1fcc4b7ab
savio04 <savio27.05.2001@gmail.com>
2024-03-10 12:13:36 -0300

    Log: Remove logs

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,6 @@
 import { app as server } from "./app";
 import { envs } from "../../../config/envs";
 
-console.log({ port: envs.PORT });
-
 server.listen(envs.PORT, () => {
   process.stdout.write(`Api is running: ${envs.PORT}\n`);
 });
